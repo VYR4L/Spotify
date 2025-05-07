4434aca9fd10d98f187b82462cb4982401e3c159
VYR4L <fkz.zanatt@gmail.com>
2025-05-06 21:52:20 -0300

    Front finalizado (JS e React)

--- Aula 1/gabarito-da-aula1/src/App.jsx
+++ Aula 1/gabarito-da-aula1/src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Artists from '../pages/Artists.jsx'
+import Artist from '../pages/Artist.jsx'
+import Home from '../pages/Home.jsx'
+import Song from '../pages/Song.jsx'
+import Songs from '../pages/Songs.jsx'
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
     <Header />
-    <Main />
-    </>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/songs" element={<Songs />} />
+      <Route path="/artists" element={<Artists />} />
+      <Route path="/artist/:id" element={<Artist />} />
+      <Route path="/song/:id" element={<Song />} />
+    </Routes>
+
+    </BrowserRouter>
   )
   
 }
