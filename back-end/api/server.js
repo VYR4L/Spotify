bb01b2994e95ce06dcc583159da5f3236121bc00
VYR4L <fkz.zanatt@gmail.com>
2025-05-11 14:59:43 -0300

    Ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import { db } from './connect.js';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
@@ -8,13 +11,23 @@ const PORT = 3000;
 app.use(cors());
 // app.use(express.json());
 
-app.get('/artists', async (req, res) => {
+app.get('/api/', (req, res) => {
+    res.send('Hello API');
+});
+
+app.get('/api/artists', async (req, res) => {
     res.send(await db.collection('artists').find().toArray());
 });
 
-app.get('/songs', async (req, res) => {
+app.get('/api/songs', async (req, res) => {
         res.send(await db.collection('songs').find().toArray());
 
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
